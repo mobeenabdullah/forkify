88d2e0723a1460ba077d37f4834f4aee0da2459c
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-08-19 16:53:37 +0500

    Implemented Pagination

--- src/js/model.js
+++ src/js/model.js
@@ -1,11 +1,13 @@
-import { API_URL } from './config.js';
+import {API_URL, RES_PER_PAGE} from './config.js';
 import { getJSON } from "./helpers.js";
 
 export const state = {
     recipe: {},
     search: {
         query: '',
-        results: []
+        results: [],
+        page: 1,
+        resultsPerPage: RES_PER_PAGE
     }
 };
 
@@ -32,7 +34,6 @@ export const loadRecipe = async function(id) {
 
 export const loadSearchResults = async function(query) {
     try {
-
         state.search.query = query;
 
         const data = await getJSON(`${API_URL}?search=${query}`);
@@ -47,6 +48,16 @@ export const loadSearchResults = async function(query) {
             }
         });
     } catch (err) {
+        console.log(`${err} 💣💣💣`)
         throw err;
     }
+}
+
+export const getSearchResultsPage = function(page = state.search.page) {
+    state.search.page = page
+
+    const start = (page -1) * state.search.resultsPerPage // 0;
+    const end = page *  state.search.resultsPerPage // 9;
+
+    return state.search.results.slice(start, end);
 }
